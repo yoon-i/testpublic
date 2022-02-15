99c97a33427e2000bb6605f0e0aea51e53bd9692
somijjjjj <gpgp0908@naver.com>
2022-02-15 16:16:02 +0900

    Edit django directory : products app

--- angelTeam/angelTeam/main/urls.py
+++ angelTeam/angelTeam/main/urls.py
@@ -3,7 +3,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     path('', views.main, name="main"),
     path('menu/',views.menu, name="menu"),
+    path('menu/product/', include('products.urls')),
 ]
