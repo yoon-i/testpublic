437d0c47bbf5bcf7fcd1022eb2b200cc4822646c
Seyoon <sstpdbs@naver.com>
2022-02-18 00:21:50 +0900

    accounts 및 html href연결

---
+++ angelTeam/angelTeam/angelTeam/static/js/keyup.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $("#keyword").keyup(function() {
+
+    })
+})
+
+$(document).ready(function() {
+    $("#keyword").keyup(function() {
+        var k = $(this).val();
+    })
+})
+
+$(document).ready(function() {
+    $("#keyword").keyup(function() {
+        var k = $(this).val();
+        $("#user-table > tbody > tr").hide();
+    })
+})
+
+$(document).ready(function() {
+    $("#keyword").keyup(function() {
+        var k = $(this).val();
+        $("#user-table > tbody > tr").hide();
+        var temp = $("#user-table > tbody > tr > td:nth-child(3n+1):contains('" + k + "')");
+
+        $(temp).parent().show();
+    })
+})
