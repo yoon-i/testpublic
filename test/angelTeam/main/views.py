d0dcce46add8d07d42341f3555535ca2e351b593
doyeon <realdodo16@gmail.com>
2022-02-17 14:42:41 +0900

    remove folder

--- test/angelTeam/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-
-# Create your views here.
-
-def main(request):
-    return render(request, 'main/main.html')
