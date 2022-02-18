4b7a218b8ee1de050e407ba86a88ab428d8ecd07
Seyoon <sstpdbs@naver.com>
2022-02-19 05:30:02 +0900

    fix&top 고정 header구조 수정 accounts폰트 추가

---
+++ angelTeam/angelTeam/angelTeam/main/urls.py
@@ -0,0 +1,14 @@
+# main > urls.py
+
+from django.urls import path, include
+from . import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.main, name="main"),
+    path('menu/', views.menu, name="menu"),
+    path('menu/product/', include('products.urls', namespace='products')),
+    path('store/', views.store, name="store"),
+    path('cs/', views.cs, name="cs"),
+]
