4727ee66ded8abfc852aad97b0e363baf0e74a67
doyeon <realdodo16@gmail.com>
2022-02-22 14:20:30 +0900

    불필요한 파일 제거 및 수정

--- angelTeam/angelTeam/static/js/keyup.js
+++ angelTeam/angelTeam/static/js/keyup.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $("#keyword").keyup(function() {
-
     })
 })
 
@@ -21,7 +20,7 @@ $(document).ready(function() {
     $("#keyword").keyup(function() {
         var k = $(this).val();
         $("#user-table > tbody > tr").hide();
-        var temp = $("#user-table > tbody > tr > td:nth-child(3n+1):contains('" + k + "')");
+        var temp = $("#user-table > tbody > tr > td:nth-child(2n+1):contains('" + k + "')");
 
         $(temp).parent().show();
     })
